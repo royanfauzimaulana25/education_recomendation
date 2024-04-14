28215936fd828b0371f6c3f902a138c2245dde1e
Muh Royan Fauzi Maulana <royanfauzimaulana25@gmail.com>
2024-04-14 22:39:35 +0700

    Update App.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ if st.button('Predict'):
     new_data = data_preprocessing(data=data)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
-    st.write("Credit Scoring: {}".format(prediction(new_data)))
+    st.write("Student Status: {}".format(prediction(new_data)))
